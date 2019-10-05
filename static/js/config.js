c06fc520b59ac773938ad9076926aecb1a85184a
andersjh <andersjh1120@gmail.com>
2019-10-04 21:16:07 -0500

    initial commit

---
+++ static/js/config.js
@@ -0,0 +1,4 @@
+// API key
+//var API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+// API key
+const API_KEY = "pk.eyJ1IjoiYW5kZXJzamgiLCJhIjoiY2sxODNlMDBzMDZ6cTNvcDFwZTh0eGJnNyJ9.UOYs4mSt5CSEuifzIxu1tA";
